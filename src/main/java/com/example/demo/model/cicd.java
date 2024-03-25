597ef55248b507be45ba153aa2174c4143c566bb
nguyen van du <du2352001@gmail.com>
2024-03-25 15:07:12 +0700

    update

---
+++ src/main/java/com/example/demo/model/cicd.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class cicd {
+    @Id
+    private String uuid;
+    private String name;
+}
